31bb767558d7f8e304464030c6151898d5a1fafe
Kof Sports <kofsports@Kofs-Air.lan>
2026-07-18 00:01:41 -0400

    Add Sanity content integration foundation

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -2,10 +2,12 @@ import { createClient } from "next-sanity";
 
 import { sanityEnv } from "@/sanity/lib/env";
 
-export const sanityClient = createClient({
+export const client = createClient({
   projectId: sanityEnv.projectId,
   dataset: sanityEnv.dataset,
   apiVersion: sanityEnv.apiVersion,
   useCdn: true,
   perspective: "published",
 });
+
+export const sanityClient = client;
